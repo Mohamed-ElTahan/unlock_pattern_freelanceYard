7af43a0c8cadf406fd2c195a6f08c45b52e5a76a
Mohamed ElTahan <mosherifeltahan@gmail.com>
2026-06-23 13:42:39 +0300

    chore: setup main app structure and add assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'unlock_pattern_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Unlock Pattern',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+        brightness: Brightness.dark,
+      ),
+      home: const UnlockPatternScreen(),
+    );
+  }
+}
